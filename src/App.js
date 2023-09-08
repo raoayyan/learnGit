1ddbf5544372fa82f05d508e4293df1f730d9296
ayyan akbar <raokhan627@gmail.com>
2023-09-08 22:43:01 +0500

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="whole">
+    <div className="Navbar">
+      <div className="Logo">
+        <h2>OpulentGold Suits</h2>
+      </div>
+      <div className="LoginButton">
+        <button>Login</button>
+      </div>
+    </div>
+      <div className="LandPage">
+    
+       
+      </div>
+      
+     <div className='item'>
+     <div className='hd'><h2>Categories</h2></div>
+      <div className="Products">
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692464972/-a-page-for-website-where-customer-can-place-order-of-suit-theme-black-and-golden-438133653_2_aeynyw.png" alt="Product 1" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692464735/aa_lxapsg.png" alt="Product 2" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692464749/-a-page-for-website-where-customer-can-place-order-of-suit-theme-black-and-golden-438133653_1_zcb74x.png" alt="Product 2" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692464738/-a-page-for-website-where-customer-can-place-order-of-suit-theme-black-and-golden-438133653_3_rz0idn.png" alt="Product 2" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692464727/-image-of-watch-just-1-or-2-in-each-image-theme-black-and-golden-8k--1232357_indjph.png" alt="Product 2" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692464725/-image-of-watch-just-1-or-2-in-each-image-theme-black-and-golden-8k--1232357_1_t7orms.png" alt="Product 2" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692464719/-a-page-for-website-where-customer-can-place-order-of-suit-theme-black-and-golden-8k--829142359_puobe4.png" alt="Product 2" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692464691/-a-page-for-website-where-customer-can-place-order-of-suit-theme-black-and-golden-8k--829142359_1_zpm7ed.png" alt="Product 2" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692464717/-a-page-for-website-where-customer-can-place-order-of-shoes-just-1-or-2-in-each-image-theme-black-an-693672710_bl20df.png" alt="Product 2" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>{" "}
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692464722/-image-of-shoes-just-1-or-2-in-each-image-theme-black-and-golden-8k--370812993_jvutjm.png" alt="Product 2" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>{" "}
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692476458/-image-of-shoes-just-1-or-2-in-each-image-theme-black-and-golden-8k--370812993_1_rmctwz.png" alt="Product 2" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>{" "}
+        <div className="ProductCard">
+          <img src="https://res.cloudinary.com/dxarzcxrv/image/upload/v1692464729/-image-of-shoes-just-1-or-2-in-each-image-theme-black-and-golden-8k--370812993_2_zjjbe5.png" alt="Product 2" />
+          <button className="AddToCartButton">Add to Cart</button>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
