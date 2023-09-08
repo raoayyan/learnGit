6bf2cbc87e9c6ea94a2074db874b43ceb6ec4b8b
ayyan akbar <raokhan627@gmail.com>
2023-09-08 23:41:33 +0500

    Revert "yeah changed"
    This reverts commit 115684a3cc95ff3edb2ca03d888d3c44a1c07143.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
       </div>
       <div className="LoginButton">
         <button>Login</button>
-        <button>Register</button>
       </div>
     </div>
       <div className="LandPage">
